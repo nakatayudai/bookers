20ac1836d8da07f4da0962acb63c33839cccd836
nakatayudai <102974156+nakatayudai@users.noreply.github.com>
2022-06-01 10:48:44 +0000

    upload

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
+
   def new
-    
+
   end
 
   def index
     @books = Book.all
-    
+
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+
+  root :to => 'homes#top'
   get 'books/new'
-  get 'top'=>'homes#top'
 
   get 'books'=>'books#index'
   post 'books'=>'books#create'
