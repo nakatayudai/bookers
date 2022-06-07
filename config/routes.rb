f0869c05abae760a2b8f9464e2a3f5dce4d31f7e
nakatayudai <102974156+nakatayudai@users.noreply.github.com>
2022-06-07 15:47:03 +0000

    2022/06/07

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
 
   root :to => 'homes#top'
-  get 'books/new'
-  get 'books/edit'
-  post 'books'=>'books#create'
-  get 'books'=>'books#index'
- 
-  get 'books/:id' => 'books#show', as: 'book'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :books
 end
